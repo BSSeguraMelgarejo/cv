fbc1c21244975c747be301e533a81e5ae8076996
BrayanSegura <u1201929@unimilitar.edu.co$ git config --global user.email u1201929@unimilitar.edu.co>
2020-08-12 04:32:26 -0500

    education complete

---
+++ education.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brayan Segura</title>
+
+	<meta charset="utf-8">
+	<meta name="description" content="This is the Brayan Segura's Curriculum Vitae"/>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="css/all.min.css" rel="stylesheet">
+
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php
+require_once('navbar.php');
+?>
+
+<main class="container">
+	<div class="row educationRow d-flex justify-content-center">
+		<h2 class="col-12 Title educationTitle">EDUCATION</div>
+		<div class="col-12">
+			<div class="row">
+				<h3 class="col-12 Title titleFormal p-3">FORMAL EDUCATION</h3>
+				<div class="col-12">
+					<div class="row d-flex justify-content-center align-items-center text-center">
+						<img class="col-6 imgEducation" src="https://www.educacionbogota.edu.co/portal_institucional/sites/default/files/2019-06/N1_250.jpg" alt="colegio republica de colombia">
+						<div class="col-6">
+							<div class="row d-flex justify-content-center">
+								<h4 class="col-12">IED Colegio República de Colombia</h4>
+								<h5 class="col-12">High School</h5>
+								<p class="col-12">Bogotá DC. Graduate in 2015</p>
+							</div>
+						</div>
+
+					</div>
+					<div class="row d-flex justify-content-center align-items-center text-center">
+						
+						<div class="col-6">
+							<div class="row">
+								<h4 class="col-12">Multimedia Engineering</h4>
+								<h5 class="col-12">Universidad Militar Nueva Granada</h5>
+								<p class="col-12">Bogotá DC. Actually</p>
+							</div>
+						</div>
+						<img class="col-6 imgEducation" src="https://scontent.fbog3-1.fna.fbcdn.net/v/t1.0-9/23380349_189081524982164_1849704108843703414_n.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=XSRMX2iwW0gAX8vBzJE&_nc_ht=scontent.fbog3-1.fna&oh=6d144b831eafa1a0f77ac4105bb4aa49&oe=5F5A446D" alt="multimedia Engineering">
+
+					</div>
+				</div>
+				
+			</div>
+
+			<div class="row">
+				<h3 class="col-12 Title titleFormal">SHORT COURSES</h3>
+				
+				<div class="col-6 p-3 text-center">
+					<h4>Key aspects to be a music manager</h4>
+					<h5>Camara de Comercio de Bogotá - 2020</h5>
+					<img class="imgEducation2" src="http://www.eltransporte.com/wp-content/uploads/2017/03/C%C3%A1mara-de-Comercio-de-Bogot%C3%A1.png" alt="camara de comercio">
+				</div>
+
+				<div class="col-6 p-3 text-center">
+					<h4>Fundamentals of SEO</h4>
+					<h5>LinkedIn Learning - 2020</h5>
+					<img class="imgEducation2" src="https://www.srsolis.com/wp-content/uploads/xP5OoeFT_400x400-e1565069219703-300x165.jpg" alt="LinkedInLearning">
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php
+require_once('footer.php');
+?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
