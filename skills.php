90775b7885ce287213269cf471c9c3fa172b3c82
BrayanSegura <u1201929@unimilitar.edu.co$ git config --global user.email u1201929@unimilitar.edu.co>
2020-08-12 05:03:09 -0500

    skills complete

---
+++ skills.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brayan Segura</title>
+
+	<meta charset="utf-8">
+	<meta name="description" content="This is the Brayan Segura's Curriculum Vitae"/>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="css/all.min.css" rel="stylesheet">
+
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php
+require_once('navbar.php');
+?>
+
+<main class="container">
+	<div class="row skills">
+		<h3 class="col-12 Title skillsTitle">SKILLS</h3>
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 skill">
+			<div class="row text-center d-flex justify-content-center">
+				<img class=" imgSkill col-12" src="img/cplusplus.svg" alt="cplusplus">
+				<h3 class="titleEachSkill">C++ / C# Programming</h3>
+				<h4 class="descSkill">Visual Studio. Level: Intermediate</h4>
+			</div>
+		</div>
+
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 skill">
+			<div class="row text-center d-flex justify-content-center">
+				<img class=" imgSkill2 col-12" src="https://www.cbronline.com/wp-content/uploads/2016/08/8TE6xogGc-e1485190543268.png" alt="opengl">
+				<h3 class="titleEachSkill">Computer graphics</h3>
+				<h4 class="descSkill">OpenGL. Level: Intermediate</h4>
+			</div>
+		</div>
+
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 skill">
+			<div class="row text-center d-flex justify-content-center">
+				<img class=" imgSkill col-12" src="https://3cthinkers.com/en/wp-content/uploads/2017/03/html-css-js.jpg" alt="Frontend">
+				<h3 class="titleEachSkill">Frontend web programming</h3>
+				<h4 class="descSkill">HTML – CSS – JS - JQuery - Bootstrap. Level: Intermediate</h4>
+			</div>
+		</div>
+
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 skill">
+			<div class="row text-center d-flex justify-content-center">
+				<img class=" imgSkill2 col-12" src="https://media.metrolatam.com/2020/03/18/1200pxadobecreat-923a18f3e6866ab7dff49afc2452a5d8.jpg" alt="adobe">
+				<h3 class="titleEachSkill">Multimedia design</h3>
+				<h4 class="descSkill">Adobe Suite. Level: High</h4>
+			</div>
+		</div>
+
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 skill">
+			<div class="row text-center d-flex justify-content-center">
+				<img class=" imgSkill col-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Calligrakrita-base.svg/600px-Calligrakrita-base.svg.png" alt="krita">
+				<h3 class="titleEachSkill">2D Animation</h3>
+				<h4 class="descSkill">Krita - Adobe Animate. Level: Basic</h4>
+			</div>
+		</div>
+
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 skill">
+			<div class="row text-center d-flex justify-content-center">
+				<img class=" imgSkill2 col-12" src="https://i.pinimg.com/564x/6c/ec/0d/6cec0d39fc88bff108129c1a69b5a86e.jpg" alt="blender">
+				<h3 class="titleEachSkill">Modeling, rendering and 3D animation</h3>
+				<h4 class="descSkill">Autodesk Maya and Blender. Level: Basic</h4>
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+
+
+
+
+<?php
+require_once('footer.php');
+?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
