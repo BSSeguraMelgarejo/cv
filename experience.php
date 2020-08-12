bfd2d612c0c8e955bbee9505f8393ea3a1dde615
BrayanSegura <u1201929@unimilitar.edu.co$ git config --global user.email u1201929@unimilitar.edu.co>
2020-08-12 05:29:12 -0500

    finish i think

---
+++ experience.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brayan Segura</title>
+
+	<meta charset="utf-8">
+	<meta name="description" content="This is the Brayan Segura's Curriculum Vitae"/>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="css/all.min.css" rel="stylesheet">
+
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php
+require_once('navbar.php');
+?>
+
+<main class="container">
+	<div class="row d-flex justify-content-center text-center p-5">
+		<h2 class="col-12 Title">
+				EXPERIENCE
+		</h2>
+		<div class="col-12 Title">
+			<h3>International Multimedia Congress - 2017</h3>
+			<p class="textExperience">Volunteer in the area of graphic design. Responsible for the design of the location
+map of the “Nueva Granada Campus”, site destined for the 2017 International
+Multimedia Congress.</p>
+		</div>
+		<div class="col-12 Title">
+			<h3>Bogotá Short Film Festival - 2017/2018/2019</h3>
+			<p class="textExperience">Volunteer in many areas of the festival. On the one hand, part of the group "Bogoshorts
+Exposes" for the graphic exhibition of the festival in places of public attendance.
+Similarly, projectionist in the Teatrino room of the Jorge Eliécer Gaitán Theater, Luis Ángel Arango Library Room and "Cine Tonalá Bogotá"</p>
+		</div>
+		<div class="col-12 Title">
+			<h3>Virtual Course, Intellectus - 2019</h3>
+			<p class="textExperience">Audiovisual postproducer for the virtual course of the memory center, Intellectus.
+Editing of explanatory videos for the course</p>
+		</div>
+		<div class="col-12 Title">
+			<h3>“Aumentando Capacidades”, Research Group - 2019</h3>
+			<p class="textExperience">Facultad de Ciencia Y Tecnología de la Universidad Autónoma de Entre Ríos
+Multimedia designer in charge of preparing the graphic material of a pictographic
+communicator with adapted electronics, for a patient diagnosed with Cerebral
+Palsy and hemiplegia on the right side.</p>
+		</div>
+	
+	<div class="col-12 Title">
+			<h3>Comercializadora MASS - 2020</h3>
+			<p class="textExperience">Development of the website of the company "comercializadora mass". Complete
+development based on frontend technologies and boostrap framework. Graphic
+design for the realization of the catalog for the same company</p>
+		</div>
+	</div>
+
+</main>
+
+<?php
+require_once('footer.php');
+?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
